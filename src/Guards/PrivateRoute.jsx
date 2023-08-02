44e5b15654b048158a92787fc4f43f882903eba5
Leo007ua <leo.oliinyk@ukr.net>
2023-08-02 10:05:15 +0300

    add PropTypes

--- src/Guards/PrivateRoute.jsx
+++ src/Guards/PrivateRoute.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectToken } from 'redux/selectors';
+import PropTypes from 'prop-types';
 
 export const PrivateRoute = ({ children }) => {
   const token = useSelector(selectToken);
   return token ? children : <Navigate to="/" />;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
 };
