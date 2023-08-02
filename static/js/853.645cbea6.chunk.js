c0766ef8a7737368852bbbdec3fde188cd987527
Leo007ua <139163325+Leo007ua@users.noreply.github.com>
2023-08-02 05:06:37 +0000

    Deploying to gh-pages from @ Leo007ua/goit-react-hw-08-phonebook@9bb20a94c02ab44eacc6ed2056714a8d0fd42fa1 🚀

---
+++ static/js/853.645cbea6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[853],{3853:function(t,e,o){o.d(e,{Ihx:function(){return n}});var r=o(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polygon",attr:{points:"22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"}}]})(t)}}}]);
+//# sourceMappingURL=853.645cbea6.chunk.js.map
